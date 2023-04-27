1370a27665f373ea7fc1d8746738af6996dc48c8
Fabrice K <kerremans.1904.student@ifosupwavre.be>
2023-04-27 17:22:25 +0200

    Ajoutez des fichiers projet.

---
+++ CarListMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CarListMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CarListMaui/Model/Carcs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarListMaui.Model
+{
+    public class Carcs
+    {
+
+        public int Id { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string vin { get; set; }
+
+    }
+
+    
+}
